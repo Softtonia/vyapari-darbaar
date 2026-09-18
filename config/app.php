94b981c2ddd6b65f37f8c0f156e27606d650894f
vkwebdev <vijay.kumar@softtonia.com>
2026-09-18 13:23:00 +0530

    cors

--- config/app.php
+++ config/app.php
@@ -54,7 +54,15 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'frontend_admin_url' => env('FRONTEND_ADMIN_URL', 'http://localhost:3000'),
+    'frontend_admin_url' => env(
+        'FRONTEND_ADMIN_URL',
+        'http://localhost:3000'
+    ),
+
+    'frontend_user_url' => env(
+        'FRONTEND_USER_URL',
+        'http://localhost:3000'
+    ),
 
     'support_email' => env('SUPPORT_EMAIL', 'support@vyaparidarbar.com'),
 
