acafcac898e87342c8a8ec8c4b8a198f1064c408
vkwebdev <vijay.kumar@softtonia.com>
2026-09-24 16:33:35 +0530

    feat: Implement CampaignEmailService and Notification for trigger events

---
+++ app/Notifications/CampaignEmailNotification.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
+
+class CampaignEmailNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $subject;
+    public $body;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $subject, string $body)
+    {
+        $this->subject = $subject;
+        $this->body = $body;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject($this->subject)
+            ->view('emails.raw_html', ['htmlBody' => new HtmlString($this->body)]);
+    }
+}
